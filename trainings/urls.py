ffbb150a4a4b063204d863e10fd240aa17ee5cd8
EMANUEL ANGELO DA SILVA ALMEIDA COUTINHO <emanuelangelo@outlook.com.br>
2026-02-24 18:01:08 -0300

    urls train

--- trainings/urls.py
+++ trainings/urls.py
@@ -2,9 +2,9 @@ from rest_framework.routers import DefaultRouter
 from .views import TrainingSessionViewSet, DrillCatalogViewSet, TrainingDrillViewSet, DrillScoreViewSet
 
 router = DefaultRouter()
-router.register(r"", TrainingSessionViewSet, basename="trainings")
 router.register(r"catalog", DrillCatalogViewSet, basename="drill-catalog")
 router.register(r"drills", TrainingDrillViewSet, basename="training-drills")
 router.register(r"scores", DrillScoreViewSet, basename="drill-scores")
+router.register(r"", TrainingSessionViewSet, basename="trainings")
 
 urlpatterns = router.urls
