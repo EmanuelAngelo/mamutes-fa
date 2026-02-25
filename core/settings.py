5ee8a28eff863d6a15a05c19acac79e5e79ac959
EMANUEL ANGELO DA SILVA ALMEIDA COUTINHO <emanuelangelo@outlook.com.br>
2026-02-25 12:42:15 -0300

    s

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production s
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
